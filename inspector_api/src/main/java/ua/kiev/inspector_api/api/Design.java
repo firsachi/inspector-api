fda2fb77c2f8f20f4c4c5e9fd5ec5a6b467e0505
firsov <firsov@firs.kievreclama.local>
2017-09-05 19:52:22 +0300

    I add image source

--- inspector_api/src/main/java/ua/kiev/inspector_api/api/Design.java
+++ inspector_api/src/main/java/ua/kiev/inspector_api/api/Design.java
@@ -1,12 +1,29 @@
 package ua.kiev.inspector_api.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
+import ua.kiev.inspector_api.model.CompleteTaskModel;
+import ua.kiev.inspector_api.model.UserPrincipalModel;
 import ua.kiev.inspector_api.service.InspectorTaskService;
 
 @RestController
@@ -19,34 +36,36 @@ public class Design {
 	@Autowired
 	private Gson gson;
 	
+	@Resource(mappedName = "fileStorage/basePath")
+	 private String basePath;
+	
 	@RequestMapping(value="objects")
 	public String getObjects(){
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 		return gson.toJson(inspectorTaskService.fullTaskUser(username));
 	}
-	/*
+	
+	
+	@RequestMapping(value = "images/{id}", method=RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE )
+	public @ResponseBody byte[] getImage(@PathVariable("id") int id) throws IOException {
+		UserPrincipalModel user = (UserPrincipalModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		CompleteTaskModel model = inspectorTaskService.getTask(id, user);
+		File file = new File(basePath, model.getPhoto());
+		byte array[];
+		if (!file.exists()) {
+		      throw new FileNotFoundException("File " + file + " does not exist");
+		  };
+		try (FileInputStream imageInFile = new FileInputStream(file)){
+			array = new byte[(int) file.length()];
+			imageInFile.read(array);
+		}
+		return array;
+	}
+	
+	
 	@GetMapping(value="objects/{id}")
 	public String getObject(@PathVariable int id){
-		UserModel user = (UserModel)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		taskService.setUserEmail(user.getUserId());
-		TaskModel taskModel = taskService.byId(id);
-		JSONObject resultJson = new JSONObject();
-		try {
-			resultJson.put("idNumber", taskModel.getIdNumber());
-			resultJson.put("district", "");
-			resultJson.put("street", taskModel.getStreet());
-			resultJson.put("bldg", taskModel.getBuild());
-			resultJson.put("location", taskModel.getLocation());
-			resultJson.put("lat", taskModel.getLat());
-			resultJson.put("lng", taskModel.getLng());
-			resultJson.put("photoURL", taskModel.getPhotoURL());
-			resultJson.put("summary", taskModel.getSummary());
-			resultJson.put("usrNotes", taskModel.getUsrNotes());
-			resultJson.put("admNotes", taskModel.getAdmNotes());
-			resultJson.put("created", taskModel.getCreated());
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return resultJson.toString();
-	}*/
+		UserPrincipalModel user = (UserPrincipalModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return gson.toJson(inspectorTaskService.getTask(id, user));
+	}
 }
